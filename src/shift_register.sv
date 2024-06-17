e3874ec4ff1007716826b1741c2de0bdc5fdff6d
Charbel SAAD <charbel.saad@etu.emse.fr>
2024-06-17 12:54:17 +0000

    Changed the incrementing perator

--- src/shift_register.sv
+++ src/shift_register.sv
@@ -15,12 +15,22 @@ module shift_register (
 	output wire[15 : 0] out_o
 );
 	reg[15 : 0] shift_s;
+	genvar i;
 
 	always @(posedge clk, negedge resetb)
 	begin
-		if(~resetb)	shift_s <= 16'b0;
-		else if(en_i)	shift_s <= {shift_s[14 : 0], in_i};
+		if(~resetb)	shift_s[0] <= 1'b0;
+		else if(en_i)	shift_s[0] <= in_i;
 	end
+
+	generate
+		for(i = 1; i < 16; i = i + 1)
+			always @(posedge clk, negedge resetb)
+			begin
+				if(~resetb)	shift_s[i] <= 1'b0;
+				else if(en_i)	shift_s[i] <= shift_s[i - 1];
+			end
+	endgenerate
 
 	assign out_o = shift_s;
 
