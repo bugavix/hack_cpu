c661aec6744ca1a7858b37e5bd9e1207a6731e9e
bugavix <ssj4.ssj373@gmail.com>
2024-07-19 09:52:37 +0200

    Changed the cpu fsm back to mealy

--- src/fsm.sv
+++ src/fsm.sv
@@ -1,4 +1,3 @@
-//A
 //------------------------------------------------------------------------
 // Module Name    : fsm
 // Creator        : Charbel SAAD
@@ -12,16 +11,9 @@
 
 `timescale 1ns/1ps
 
-typedef enum reg[3 : 0] {
+typedef enum reg[1 : 0] {
 	FETCH_INSTRUCTION,
-	NO_LATCH,
-	LATCH_D,
-	LATCH_A,
-	LATCH_AD,
-	LATCH_PC,
-	LATCH_PCD,
-	LATCH_PCA,
-	LATCH_PCAD,
+	LATCH,
 	FETCH_MEMORY,
 	SAVE_MEMORY
 } fsm_state_t;
@@ -31,17 +23,11 @@ module fsm (
 	input wire clk, resetb, halt_i, zr_i, zn_i,
 	output reg spiStart_o, rwb_o, selSPIAddress_o, selSPIDest_o, enA_o, enD_o, enPC_o, loadPC_o,
 	output wire selA_o, selALU_o, na_o, za_o, nb_o, zb_o, f_o, no_o,
-	output wire[3 : 0] state_o
+	output wire[1 : 0] state_o
 );
 
 	fsm_state_t state_s;
 	fsm_state_t next_state_s;
-	wire loadPC_s, enA_s, enD_s;
-
-	assign loadPC_s = instruction_i[15] & ((zn_i & instruction_i[2]) | (zr_i & instruction_i[1]) | (instruction_i[0] & ~zn_i & ~zr_i));
-	assign enA_s = ~instruction_i[15] | instruction_i[5];
-	assign enD_s = instruction_i[15] & instruction_i[4];
-	// TODO: find a more optimized way of doing this.
 
 	always_ff @(posedge clk, negedge resetb)
 	begin
@@ -57,69 +43,20 @@ module fsm (
 					next_state_s = FETCH_MEMORY;
 				else if(instruction_i[15] & instruction_i[3])
 					next_state_s = SAVE_MEMORY;
-				else case({loadPC_s, enA_s, enD_s})
-					3'b000: next_state_s = NO_LATCH;
-					3'b001: next_state_s = LATCH_D;
-					3'b010: next_state_s = LATCH_A;
-					3'b011: next_state_s = LATCH_AD;
-					3'b100: next_state_s = LATCH_PC;
-					3'b101: next_state_s = LATCH_PCD;
-					3'b110: next_state_s = LATCH_PCA;
-					3'b111: next_state_s = LATCH_PCAD;
-				endcase
+				else
+					next_state_s = LATCH;
 
-			NO_LATCH:
-				next_state_s = FETCH_INSTRUCTION;
-
-			LATCH_D:
-				next_state_s = FETCH_INSTRUCTION;
-
-			LATCH_A:
-				next_state_s = FETCH_INSTRUCTION;
-
-			LATCH_AD:
-				next_state_s = FETCH_INSTRUCTION;
-
-			LATCH_PC:
-				next_state_s = FETCH_INSTRUCTION;
-
-			LATCH_PCD:
-				next_state_s = FETCH_INSTRUCTION;
-
-			LATCH_PCA:
-				next_state_s = FETCH_INSTRUCTION;
-
-			LATCH_PCAD:
+			LATCH:
 				next_state_s = FETCH_INSTRUCTION;
 
 			FETCH_MEMORY:
 				if(instruction_i[3])
 					next_state_s = SAVE_MEMORY;
-				else case({loadPC_s, enA_s, enD_s})
-					3'b000: next_state_s = NO_LATCH;
-					3'b001: next_state_s = LATCH_D;
-					3'b010: next_state_s = LATCH_A;
-					3'b011: next_state_s = LATCH_AD;
-					3'b100: next_state_s = LATCH_PC;
-					3'b101: next_state_s = LATCH_PCD;
-					3'b110: next_state_s = LATCH_PCA;
-					3'b111: next_state_s = LATCH_PCAD;
-				endcase
+				else
+					next_state_s = LATCH;
 
 			SAVE_MEMORY:
-				case({loadPC_s, enA_s, enD_s})
-					3'b000: next_state_s = NO_LATCH;
-					3'b001: next_state_s = LATCH_D;
-					3'b010: next_state_s = LATCH_A;
-					3'b011: next_state_s = LATCH_AD;
-					3'b100: next_state_s = LATCH_PC;
-					3'b101: next_state_s = LATCH_PCD;
-					3'b110: next_state_s = LATCH_PCA;
-					3'b111: next_state_s = LATCH_PCAD;
-				endcase
-
-			default:
-				next_state_s = FETCH_INSTRUCTION;
+				next_state_s = LATCH;
 		endcase
 	end
 
@@ -138,100 +75,28 @@ module fsm (
 				enPC_o = 1'b0;
 			end
 
-			NO_LATCH:
+			LATCH:
 			begin
 				spiStart_o = 1'b0;
 				rwb_o = 1'b0; // don't care
 				selSPIAddress_o = 1'b0; // don't care
 				selSPIDest_o = 1'b0; // don't care
-				enA_o = 1'b0;
-				enD_o = 1'b0;
-				loadPC_o = 1'b0;
-				enPC_o = 1'b1;
-			end
-
-			LATCH_D:
-			begin
-				spiStart_o = 1'b0;
-				rwb_o = 1'b0; // don't care
-				selSPIAddress_o = 1'b0; // don't care
-				selSPIDest_o = 1'b0; // don't care
-				enA_o = 1'b0;
-				enD_o = 1'b1;
-				loadPC_o = 1'b0;
-				enPC_o = 1'b1;
-			end
-
-			LATCH_A:
-			begin
-				spiStart_o = 1'b0;
-				rwb_o = 1'b0; // don't care
-				selSPIAddress_o = 1'b0; // don't care
-				selSPIDest_o = 1'b0; // don't care
-				enA_o = 1'b1;
-				enD_o = 1'b0;
-				loadPC_o = 1'b0;
-				enPC_o = 1'b1;
-			end
-
-			LATCH_AD:
-			begin
-				spiStart_o = 1'b0;
-				rwb_o = 1'b0; // don't care
-				selSPIAddress_o = 1'b0; // don't care
-				selSPIDest_o = 1'b0; // don't care
-				enA_o = 1'b1;
-				enD_o = 1'b1;
-				loadPC_o = 1'b0;
-				enPC_o = 1'b1;
-			end
-
-			LATCH_PC:
-			begin
-				spiStart_o = 1'b0;
-				rwb_o = 1'b0; // don't care
-				selSPIAddress_o = 1'b0; // don't care
-				selSPIDest_o = 1'b0; // don't care
-				enA_o = 1'b0;
-				enD_o = 1'b0;
-				loadPC_o = 1'b1;
-				enPC_o = 1'b1;
-			end
-
-			LATCH_PCD:
-			begin
-				spiStart_o = 1'b0;
-				rwb_o = 1'b0; // don't care
-				selSPIAddress_o = 1'b0; // don't care
-				selSPIDest_o = 1'b0; // don't care
-				enA_o = 1'b0;
-				enD_o = 1'b1;
-				loadPC_o = 1'b1;
-				enPC_o = 1'b1;
-			end
-
-			LATCH_PCA:
-			begin
-				spiStart_o = 1'b0;
-				rwb_o = 1'b0; // don't care
-				selSPIAddress_o = 1'b0; // don't care
-				selSPIDest_o = 1'b0; // don't care
-				enA_o = 1'b1;
-				enD_o = 1'b0;
-				loadPC_o = 1'b1;
-				enPC_o = 1'b1;
-			end
-
-			LATCH_PCAD:
-			begin
-				spiStart_o = 1'b0;
-				rwb_o = 1'b0; // don't care
-				selSPIAddress_o = 1'b0; // don't care
-				selSPIDest_o = 1'b0; // don't care
-				enA_o = 1'b1;
-				enD_o = 1'b1;
-				loadPC_o = 1'b1;
-				enPC_o = 1'b1;
+				if(halt_i)
+					enPC_o = 1'b0;
+				else
+					enPC_o = 1'b1;
+				if(~instruction_i[15] | instruction_i[5])
+					enA_o = 1'b1;
+				else
+					enA_o = 1'b0;
+				if(instruction_i[15] & instruction_i[4])
+					enD_o = 1'b1;
+				else
+					enD_o = 1'b0;
+				if(instruction_i[15] & ((zn_i & instruction_i[2]) | (zr_i & instruction_i[1]) | (instruction_i[0] & ~zn_i & ~zr_i)))
+					loadPC_o = 1'b1;
+				else
+					loadPC_o = 1'b0;
 			end
 
 			FETCH_MEMORY:
@@ -256,18 +121,6 @@ module fsm (
 				enD_o = 1'b0;
 				loadPC_o = 1'b0;
 				enPC_o = 1'b0;
-			end
-
-			default:
-			begin
-				spiStart_o = 1'b0; // don't care
-				rwb_o = 1'b0; // don't care
-				selSPIAddress_o = 1'b0; // don't care
-				selSPIDest_o = 1'b0; // don't care
-				enA_o = 1'b0; // don't care
-				enD_o = 1'b0; // don't care
-				loadPC_o = 1'b0; // don't care
-				enPC_o = 1'b0; // don't care
 			end
 		endcase
 	end
